3f351c3f801cddbcc05c10a16495dd5624bef55e
blajockz <69370048+blajockz@users.noreply.github.com>
2022-06-10 13:21:53 +0100

    Create bj.py

---
+++ bj.py
@@ -0,0 +1,6 @@
+///////////////////////////////////////////////////
+print("Hello, i am blajockz, i am interested in python language and i am still a beginner at the time,")
+print("What's your name?")
+ans = input()
+print("Hello", ans, "Nice to meet you :)")
+////////////////////////////////////////////////////
